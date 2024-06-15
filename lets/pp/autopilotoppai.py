df6cc940492ce349889933e5e3cd12ef4764df41
전강현 <aniren@debian.moe>
2024-06-15 21:03:18 +0900

    oppai 명령줄 수정

--- lets/pp/autopilotoppai.py
+++ lets/pp/autopilotoppai.py
@@ -127,7 +127,7 @@ class oppai:
 			if self.gameMode != gameModes.STD and self.gameMode != gameModes.TAIKO:
 				raise exceptions.unsupportedGameModeException()
 
-			command = "B:/redstar/lets/pp/oppai-ng/oppai {}".format(mapFile)
+			command = fixPath("pp/oppai-ng/oppai {}".format(mapFile))
 			if not self.tillerino:
 				# force acc only for non-tillerino calculation
 				# acc is set for each subprocess if calculating tillerino-like pp sets
