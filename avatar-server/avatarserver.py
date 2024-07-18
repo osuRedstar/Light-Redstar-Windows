25b52190855145b1329d6f42df6b94dd79edb797
전강현 <aniren@debian.moe>
2024-07-18 23:28:44 +0900

    Update avatarserver.py

--- avatar-server/avatarserver.py
+++ avatar-server/avatarserver.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file, jsonify, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 #render_template추가
 import urllib.request
 import requests
@@ -7,14 +8,14 @@ import json
 
 import os
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_proto=1, x_host=1, x_port=1, x_prefix=1) #ProxyFix 미들웨어 추가
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 avatar_dir = "avatars" # no slash
 avatar2_dir = "bancho" # no slash
 
 # create avatars directory if it does not exist
-if not os.path.exists(avatar_dir):
-	os.makedirs(avatar_dir)
+if not os.path.exists(avatar_dir): os.makedirs(avatar_dir)
 	
 @app.route("/status")
 def serverStatus():
@@ -53,21 +54,14 @@ def serveAvatar2(uid):
 
 @app.route("/bancho/u/<string:username>")
 def serveAvatar3(username):
-	##key = random.choice(api)
-	
+	#key = random.choice(api)
 	API_key = '4713134cf26236be8cdad80768b50168feadf56f'
-
-	reqjson = requests.get(url=f"https://osu.ppy.sh/api/get_user?k={API_key}&u={username}").json()
-	uid = reqjson[0]['user_id']
-	# Check if avatar exists
-	""" if os.path.isfile("{}/{}.png".format(avatar2_dir, uid)):
+	uid = requests.get(url=f"https://osu.ppy.sh/api/get_user?k={API_key}&u={username}").json()[0]['user_id']
+	if False and os.path.isfile("{}/{}.png".format(avatar2_dir, uid)): #Check if avatar exists
 		avatarid = uid
 	else:
 		urllib.request.urlretrieve(f"https://a.ppy.sh/{uid}", f"bancho/{uid}.png")
-		avatarid = uid """
-
-	urllib.request.urlretrieve(f"https://a.ppy.sh/{uid}", f"bancho/{uid}.png")
-	avatarid = uid	
+		avatarid = uid
 
 	# Serve actual avatar or default one
 	return send_file("{}/{}.png".format(avatar2_dir, avatarid))
@@ -75,34 +69,27 @@ def serveAvatar3(username):
 ##############################################################################
 
 @app.route("/")
-def index():
-	return send_file("{}/{}.png".format(avatar_dir, '-1'))
+def index(): return send_file("{}/{}.png".format(avatar_dir, '-1'))
 
 #@app.route("/-1")
-#def index():
-#	return send_file("{}/{}.png".format(avatar_dir, '-1'))
+#def index(): return send_file("{}/{}.png".format(avatar_dir, '-1'))
+
+@app.route("/<string:f>")
+def serveAvatar4(f):
+	if os.path.isfile("{}/{}".format(avatar_dir, f)): avatarid = f
+	else: avatarid = "-404.png"
+	return send_file("{}/{}".format(avatar_dir, avatarid))
 
 @app.route('/favicon.ico')
-def favicon():
-    return send_file("{}/{}.png".format(avatar_dir, '-1'))
+def favicon(): return send_file("{}/-1.png".format(avatar_dir))
 
 @app.route("/docs")
-def	docs():
-	return render_template('docs.html')
-
-@app.route("/-1.png")
-def defaultImage():
-	# Serve actual avatar or default one
-	return send_file("{}/-1.png".format(avatar_dir))
-
+def	docs(): return render_template('docs.html')
 
 ###############################################################################
 
-
 @app.errorhandler(404)
-def page_not_found(error):
-    return send_file("{}/-404.png".format(avatar_dir))
+def page_not_found(error): return send_file("{}/-404.png".format(avatar_dir))
 
-	
 # Run the server
 app.run(host="0.0.0.0", port=5000)
