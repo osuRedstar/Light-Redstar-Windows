68425be0e8a3b7e1301d4bf62cc7b2ea5411bf68
전강현 <aniren@debian.moe>
2024-08-13 03:05:10 +0900

    Update 쿼리.sql

--- SQL/쿼리/쿼리.sql
+++ SQL/쿼리/쿼리.sql
@@ -3,3 +3,4 @@ show status like 'Max_used_connections';
 
 SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST;
 CHECK TABLE global_priv;
+#REPAIR TABLE `mysql`.`user`, `mysql`.`db`, `mysql`.`tables_priv`, `mysql`.`columns_priv`, `mysql`.`procs_priv`, `mysql`.`global_priv`;
