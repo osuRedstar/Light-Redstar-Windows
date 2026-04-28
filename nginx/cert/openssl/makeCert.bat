06efdca06b945dafe580d7174518a218ce40ab9a
전강현 <aniren@debian.moe>
2026-04-28 18:59:37 +0900

    nginx | B드라이브 --> C드라이브 이동 및 레포 분리

--- nginx/cert/openssl/makeCert.bat
+++
@@ -1,8 +0,0 @@
-REM key 생성
-openssl genrsa -out cert.key 2048
-REM csr 생성
-openssl req -new -key cert.key -out cert.csr -config makeCert.conf
-REM 인증서 발급
-openssl x509 -req -days 36524 -extensions v3_user -in cert.csr -signkey cert.key -out cert.crt -extfile makeCert.conf
-del /f /q cert.csr
-pause
